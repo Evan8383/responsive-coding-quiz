f302cf8fd11ff233f073b419514ac0454232c0c3
Evan8383 <evanbennett83@gmail.com>
2023-10-04 14:01:07 -0400

    rough question card structure built out

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,12 @@
 const startButton = document.querySelector('#startButton');
 
-startButton.addEventListener('click', () =>{
+startButton.addEventListener('click', () => {
   const welcomeContainer = document.querySelector('#welcomeContainer')
   welcomeContainer.classList.remove('active')
+  runQuiz()
 })
+
+function runQuiz() {
+  const questionContainer = document.querySelector('#questionContainer')
+  questionContainer.classList.add('active')
+}
