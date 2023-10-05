6bd16054d7ae12d3258214d5edcde01b38100aec
evan8383 <evanbennett83@gmai.com>
2023-10-04 21:45:53 -0400

    Resolved issue where score would add an additional point after the quiz completed. Timer, click handling, and score all working.

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,34 @@
 const startButton = document.querySelector('#startButton');
 const timeRemaining = document.querySelector('#timeRemaining')
 const answerOptions = document.querySelectorAll('.option')
+const displayedQuestion = document.querySelector('#question')
 let secondsLeft = 60
 let questionIndex = 0
+let score = 0
 // timer seconds and textContent
 
 const answerBank = [
   {
-    option1: 'correct',
-    option2: 'wrong',
-    option3: 'wrong',
-    option4: 'wrong',
+    question: "Javascript is an _______ language?",
+    option1: 'Objected-Oriented',
+    option2: 'Random',
+    option3: 'thats not right',
+    option4: 'the correct answer',
   },
   {
+    question: "asdfasdfasdfasdfasdf",
     option1: 'correct',
     option2: 'asdf',
-    option3: 'wrong',
-    option4: 'wrong',
-  }
+    option3: 'wqwerqwer',
+    option4: 'wqwerqwerqwerg',
+  },
+  {
+    question: "Javascript is an _______ language?",
+    option1: 'Objected-Oriented',
+    option2: 'Random',
+    option3: 'thats not right',
+    option4: 'the correct answer',
+  },
 ]
 
 
@@ -38,7 +49,7 @@ function startTimer() {
   const quizTimer = setInterval(() => {
     timeRemaining.textContent = secondsLeft
     secondsLeft--
-    if (!secondsLeft) {
+    if (secondsLeft == 0) {
       clearInterval(quizTimer);
       endGame()
     }
@@ -46,45 +57,53 @@ function startTimer() {
 }
 
 // Displays current question on the screen. Handles correct and incorrect answers
+
 function showQuestion() {
   // references the answerBank array and selects the index based on questionIndex value
   const currentQuestion = answerBank[questionIndex]
-  if (currentQuestion) {
-    const { option1, option2, option3, option4 } = currentQuestion
-    const options = [option1, option2, option3, option4]
+  if (!currentQuestion) return endGame()
 
-    // Shuffle the options array to randomize the order
-    for (let i = options.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [options[i], options[j]] = [options[j], options[i]];
-    }
-    // Assign shuffled options to answer options
-    answerOptions.forEach((option, index) => {
-      option.textContent = options[index];
+  const { option1, option2, option3, option4, question } = currentQuestion
+  const options = [option1, option2, option3, option4]
 
-      // handles click event for line items displayed 
-      option.addEventListener('click', (event) => {
-        if (event.target.textContent == option1) {
-          // TODO: handle correct selections.
-          // ! correct selections should add to the players score
-          // ! correct selections should advance the player to the next question
-
-          setTimeout(() => {
-            questionIndex++
-            showQuestion()
-          },1000);
-        } else {
-          // TODO: handle incorrect selections
-          // ! wrong selections should subtract time from the clock and show that the answer was wrong
-          // ! wrong answers should not advance the player to the next question
-        }
-      });
-    });
-  } else {
-    endGame()
+  // Shuffle the options array to randomize the order
+  for (let i = options.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [options[i], options[j]] = [options[j], options[i]];
   }
+  // Assign shuffled options to answer options
+  answerOptions.forEach((option, index) => {
+    // displays question and the possible options dynamically based on the answerBank object
+    option.textContent = options[index];
+    displayedQuestion.textContent = question
+
+    option.classList.remove('wrong')
+    option.classList.remove('correct')
+
+
+    // handles click event for line items displayed 
+    option.addEventListener('click', (event) => {
+      if (event.target.textContent === option1) {
+        option.classList.remove('wrong')
+        option.classList.add('correct')
+
+        event.stopImmediatePropagation()
+        
+        setTimeout(() => {
+          score++
+          questionIndex++
+          console.log(score)
+          showQuestion()
+        }, 300);
+      } else {
+        secondsLeft -= 2
+        option.classList.add('wrong')
+      }
+    });
+  });
 }
 
 function endGame() {
+  console.log(score)
   console.log('Game Over');
 }
