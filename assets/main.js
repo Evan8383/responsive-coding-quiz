04ab68b12a40bb506a0684f130fd29593f583411
Evan8383 <evanbennett83@gmail.com>
2023-10-06 08:55:33 -0400

    organized js file so document followed a consistant structure. Added score saved notification. Added replaced arrow function with saveScore function.

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,27 @@
-const startButton = document.querySelector('#startButton');
-const timeRemaining = document.querySelector('#timeRemaining');
-const answerOptions = document.querySelectorAll('.option');
-const displayedQuestion = document.querySelector('#question');
 const welcomeContainer = document.querySelector('#welcomeContainer');
 const questionContainer = document.querySelector('#questionContainer');
 const endGameContainer = document.querySelector('#endGameContainer');
+
+const scoreboard = document.querySelector('#scoreboard')
+const displayScore = document.querySelector('#displayScore')
+const playerInitials = document.querySelector('#playerInitials')
+
+const timeRemaining = document.querySelector('#timeRemaining');
+const answerOptions = document.querySelectorAll('.option');
+const displayedQuestion = document.querySelector('#question');
+
 const playAgainButton = document.querySelector('#playAgainButton');
+const startButton = document.querySelector('#startButton');
+const saveScoreButton = document.querySelector('#saveScoreButton')
+
+
+const scoreList = document.querySelector('#scoreList')
 
 let secondsLeft = 60;
 let questionIndex = 0;
 let score = 0;
 let quizTimer;
-
-const displayScore = document.querySelector('#displayScore')
-const saveScoreButton = document.querySelector('#saveScoreButton')
-const playerInitials = document.querySelector('#playerInitials')
-const scoreboard = document.querySelector('#scoreboard')
-const scoreList = document.querySelector('#scoreList')
 let scoreSaved = false
-// open scoreboard buttons
-
 
 const answerBank = [
   {
@@ -45,13 +47,6 @@ const answerBank = [
   },
 ]
 
-startButton.addEventListener('click', () => {
-  welcomeContainer.classList.remove('active')
-  // Make the question card visible
-  startTimer()
-  showQuestion()
-})
-
 function startTimer() {
   timeRemaining.textContent = secondsLeft
   quizTimer = setInterval(() => {
@@ -65,59 +60,6 @@ function startTimer() {
 }
 function stopTimer() {
   clearInterval(quizTimer);
-}
-
-// Displays current question on the screen. Handles correct and incorrect answers
-
-function showQuestion() {
-  // references the answerBank array and selects the index based on questionIndex value
-  questionContainer.classList.add('active')
-  const currentQuestion = answerBank[questionIndex]
-  if (!currentQuestion) {
-    stopTimer()
-    return endGame()
-  }
-
-  const { option1, option2, option3, option4, question } = currentQuestion
-  const options = [option1, option2, option3, option4]
-
-  // Shuffle the options array to randomize the order
-  for (let i = options.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [options[i], options[j]] = [options[j], options[i]];
-  }
-  // Assign shuffled options to answer options
-  answerOptions.forEach((option, index) => {
-    // displays question and the possible options dynamically based on the answerBank object
-    option.textContent = options[index];
-    displayedQuestion.textContent = question
-
-    option.classList.remove('wrong')
-    option.classList.remove('correct')
-
-    // handles click event for line items displayed 
-    option.addEventListener('click', (event) => {
-      if (event.target.textContent == answerBank[questionIndex].option1) {
-        option.classList.remove('wrong')
-        option.classList.add('correct')
-
-        event.stopImmediatePropagation()
-
-        setTimeout(() => {
-          score++
-          questionIndex++
-          console.log(score)
-          showQuestion()
-        }, 300);
-      }
-      if (event.target.textContent != answerBank[questionIndex].option1) {
-        event.stopImmediatePropagation()
-
-        secondsLeft = secondsLeft - 2
-        option.classList.add('wrong')
-      }
-    });
-  });
 }
 function restartQuiz() {
   endGameContainer.classList.remove('active')
@@ -133,42 +75,101 @@ function endGame() {
   displayScore.textContent = score
   scoreSaved = false
 }
-// Navigate to scoreboard
 function goToScoreboard() {
   welcomeContainer.classList.remove('active')
   endGameContainer.classList.remove('active')
   scoreboard.classList.add('active')
 }
+function goToMenu() {
+  endGameContainer.classList.remove('active')
+  scoreboard.classList.remove('active')
+  welcomeContainer.classList.add('active')
+}
+function showQuestion() {
+  // references the answerBank array and selects the index based on questionIndex value
+  questionContainer.classList.add('active')
+  const currentQuestion = answerBank[questionIndex]
+  if (!currentQuestion) {
+    stopTimer()
+    return endGame()
+  }
+  
+  const { option1, option2, option3, option4, question } = currentQuestion
+  const options = [option1, option2, option3, option4]
+  
+  // Shuffle the options array to randomize the order
+  for (let i = options.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [options[i], options[j]] = [options[j], options[i]];
+  }
+  // Assign shuffled options to answer options
+  answerOptions.forEach((option, index) => {
+    // displays question and the possible options dynamically based on the answerBank object
+    option.textContent = options[index];
+    displayedQuestion.textContent = question
+    
+    option.classList.remove('wrong')
+    option.classList.remove('correct')
+    
+    // handles click event for line items displayed 
+    option.addEventListener('click', (event) => {
+      if (event.target.textContent == answerBank[questionIndex].option1) {
+        option.classList.remove('wrong')
+        option.classList.add('correct')
+        
+        event.stopImmediatePropagation()
+        
+        setTimeout(() => {
+          score++
+          questionIndex++
+          console.log(score)
+          showQuestion()
+        }, 300);
+      }
+      if (event.target.textContent != answerBank[questionIndex].option1) {
+        event.stopImmediatePropagation()
+        
+        secondsLeft = secondsLeft - 2
+        option.classList.add('wrong')
+      }
+    });
+  });
+}
+function saveScore(){
+  if (scoreSaved === true) {
+    playerInitials.value = ''
+    alert('Score already saved')
+  } else {
+    event.stopImmediatePropagation()
+    const newScoreElement = document.createElement('li')
+    scoreList.appendChild(newScoreElement)
+    newScoreElement.textContent = `Player: ${playerInitials.value} - Score: ${score}`
+    playerInitials.value = ''
+    scoreSaved = true;
+    document.querySelector('#scoreSavedAlert').textContent = "Score saved"
+  }
+  setTimeout(()=>{
+    document.querySelector('#scoreSavedAlert').textContent = ""
+  }, 2500)
+}
+
+startButton.addEventListener('click', () => {
+  welcomeContainer.classList.remove('active')
+  // Make the question card visible
+  startTimer()
+  showQuestion()
+})
 const goToScoreboardBtn = document.querySelectorAll('.goToScoreboardBtn')
 goToScoreboardBtn.forEach(button => {
   button.addEventListener('click', goToScoreboard)
 })
 
 // Navigate to menu 
-function goToMenu() {
-  endGameContainer.classList.remove('active')
-  scoreboard.classList.remove('active')
-  welcomeContainer.classList.add('active')
-}
 const goToMenuBtn = document.querySelectorAll('.goToMenuBtn')
 goToMenuBtn.forEach(button => {
   button.addEventListener('click', goToMenu)
 })
 
-saveScoreButton.addEventListener('click', (event) => {
-  if (scoreSaved === true) {
-    playerInitials.value = ''
-    alert('Score already saved')
-  } else {
-    event.stopImmediatePropagation()
-    console.log(playerInitials.value)
-    const newScore = document.createElement('li')
-    scoreList.appendChild(newScore)
-    newScore.textContent = `Player: ${playerInitials.value} - Score: ${score}`
-    playerInitials.value = ''
-    scoreSaved = true;
-  }
-})
+saveScoreButton.addEventListener('click', saveScore)
 
 playAgainButton.addEventListener('click', restartQuiz)
-endGame()
